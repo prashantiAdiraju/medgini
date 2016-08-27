adc34b0ac0adf3aca3209fdd19a07a520c3ac91e
Sowmitra Deshmukh <sowmitraa@gmail.com>
2016-08-27 08:52:28 -0400

    27th

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -14,6 +14,7 @@ $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $date = strip_tags(htmlspecialchars($_POST['date']));
+$message =strip_tags(htmlspecialchars($_POST['message']));;
    
 // Create the email and send the message
 $to = 'support@medginix.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
